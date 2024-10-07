b96dc9d0a129b97df704978f3fc8bae6587349cd
Heloísa Santana da Silva <heloisasantana.silva@hotmail.com>
2024-10-07 12:30:18 -0300

    Inserindo author

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 /*
- *@author:Heloísa Santana da Silva
+ *@author:Heloísa Santana da Sila
  */
 public class MainActivity extends AppCompatActivity {
 
